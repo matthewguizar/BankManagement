b13cba665e068a212e0ece89f707ce90065ff29d
Matthew Guizar <matthewrguizar@gmail.com>
2022-08-15 10:07:56 -0700

    working

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,6 +15,9 @@ public class Main {
         Chequing chequing = new Chequing("1", "Michael Scott", 1524.51);
         Savings savings = new Savings("2", "Saul Goodman", 2241.60);
         Loan loan = new Loan("3", "Pheobe Buffay", 2537.31);
+        System.out.println(loan);
+        System.out.println(savings);
+        System.out.println(chequing);
 
 
     }
