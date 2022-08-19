ab69992848aad9a25b5ebe6cf8b9f5474fb0b687
Matthew Guizar <matthewrguizar@gmail.com>
2022-08-19 10:43:07 -0700

    gettransactions method

--- src/main/model/Bank.java
+++ src/main/model/Bank.java
@@ -1,6 +1,8 @@
 package src.main.model;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import src.main.model.account.Account;
 
@@ -18,4 +20,18 @@ public class Bank {
     public void addAccount(Account account){
         accounts.add(account.clone());
     }
+
+    public void addTransaction(Transaction transaction){
+        transactions.add(new Transaction(transaction));
+    }
+
+    public Transaction[] getTransactions(String accountId){
+        //
+        List<Transaction> list =this.transactions.stream() //streaming all elements in transaction
+        .filter((transaction ) -> transaction.getId().equals(accountId)) //filtering through accountIds that match one passed in
+        .collect(Collectors.toList());//getting as list using collectors class
+        return list.toArray(new Transaction[list.size()]); //using toArray and putting in what type to cast it into and size
+
+    }
+
 }
