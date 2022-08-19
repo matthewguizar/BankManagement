a74039dd238c0d85a2a0d686c0df6979c87056f2
Matthew Guizar <matthewrguizar@gmail.com>
2022-08-19 08:55:42 -0700

    added transaction class

--- src/main/model/Transaction.java
+++ src/main/model/Transaction.java
@@ -1,7 +1,50 @@
 package src.main.model;
 
 public class Transaction {
+    private enum Type{WITHDRAW, DEPOSIT;}
+    private Type type;
+    private long timestamp;
+    private String id;
+    private double amount;
 
+    public Transaction(Type type, long timestamp, String id, double amount){
+        this.type = type;
+        this.timestamp = timestamp;
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public Type getType() {
+        return this.type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public long getTimestamp() {
+        return this.timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
   
 
 }
