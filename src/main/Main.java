c644b0ea0120710b8a40c9fd3f93e76c1334bc77
Matthew Guizar <matthewrguizar@gmail.com>
2022-08-22 07:31:22 -0700

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,8 @@ package src.main;
 
 import java.util.concurrent.TimeUnit;
 
+import src.main.model.Bank;
+import src.main.model.Transaction;
 import src.main.model.account.Account;
 import src.main.model.account.Chequing;
 import src.main.model.account.Loan;
@@ -11,13 +13,37 @@ public class Main {
 
    static String ACCOUNTS_FILE = "src/main/data/accounts.txt";            
    static String TRANSACTIONS_FILE = "src/main/data/transactions.txt";
+   private static Bank bank = new Bank();
 
     public static void main(String[] args) {
-        Chequing chequing = new Chequing("f84c43f4-a634-4c57-a644-7602f8840870", "Michael Scott", 1524.51);
-        Account chequingCopy = chequing.clone();
-      
-        Savings savings = new Savings("ce07d7b3-9038-43db-83ae-77fd9c0450c9", "Saul Goodman", 2241.60);
-        Account savingsCopy = savings.clone();
+        Account[] accounts = new Account[] {
+            new Chequing("f84c43f4-a634-4c57-a644-7602f8840870", "Michael Scott", 1524.51),
+            new Savings("ce07d7b3-9038-43db-83ae-77fd9c0450c9", "Saul Goodman", 2241.60)
+        };
+
+        for (Account account : accounts) {
+            bank.addAccount(account);
+        }
+
+        Transaction[] transactions = new Transaction[] {
+            new Transaction(Transaction.Type.WITHDRAW, 1546905600, "f84c43f4-a634-4c57-a644-7602f8840870", 624.99),
+            new Transaction(Transaction.Type.DEPOSIT, 1578700800, "f84c43f4-a634-4c57-a644-7602f8840870", 441.93),
+            new Transaction(Transaction.Type.WITHDRAW, 1547078400, "f84c43f4-a634-4c57-a644-7602f8840870", 546.72),
+            new Transaction(Transaction.Type.WITHDRAW, 1546732800, "f84c43f4-a634-4c57-a644-7602f8840870", 546.72),
+            new Transaction(Transaction.Type.DEPOSIT, 1578355200, "f84c43f4-a634-4c57-a644-7602f8840870", 635.95),
+            new Transaction(Transaction.Type.WITHDRAW, 1547078400, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 875.64),
+            new Transaction(Transaction.Type.WITHDRAW, 1578614400, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 912.45),
+            new Transaction(Transaction.Type.WITHDRAW, 1577836800, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 695.09),
+            new Transaction(Transaction.Type.WITHDRAW, 1609459200, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 917.21),
+            new Transaction(Transaction.Type.WITHDRAW, 1578096000, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 127.94),
+            new Transaction(Transaction.Type.WITHDRAW, 1546819200, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 612.52)
+        };
+
+        for (Transaction transaction : transactions) {
+            bank.addTransaction(transaction);
+        }
+        
+        Transaction[] filteredTransactions = bank.getTransactions("f84c43f4-a634-4c57-a644-7602f8840870");
 
 
     }
